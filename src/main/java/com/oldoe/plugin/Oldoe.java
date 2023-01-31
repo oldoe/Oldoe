41cba86a0268e1ee009d833db1562457686ad765
Spencer Burgess <40410913+SpencerBurgess@users.noreply.github.com>
2023-01-31 09:11:26 -0700

    Added plot. Cleaned up main java file

--- src/main/java/com/oldoe/plugin/Oldoe.java
+++ src/main/java/com/oldoe/plugin/Oldoe.java
@@ -1,22 +1,24 @@
 package com.oldoe.plugin;
 
 import com.oldoe.plugin.commands.HomeCommand;
+import com.oldoe.plugin.commands.PlotCommand;
 import com.oldoe.plugin.commands.SetHomeCommand;
 import com.oldoe.plugin.commands.SpawnCommand;
 import com.oldoe.plugin.database.MYSQLConnector;
 import com.oldoe.plugin.listeners.*;
-import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Oldoe extends JavaPlugin implements Listener {
 
     private static MYSQLConnector dbConnector = null;
     private FileConfiguration config = getConfig();
+
+    public static MYSQLConnector GetDatabase() {
+        return dbConnector;
+    }
 
     @Override
     public void onEnable() {
@@ -29,23 +31,11 @@ public class Oldoe extends JavaPlugin implements Listener {
         this.getCommand("spawn").setExecutor(new SpawnCommand());
         this.getCommand("sethome").setExecutor(new SetHomeCommand());
         this.getCommand("home").setExecutor(new HomeCommand());
+        this.getCommand("plot").setExecutor(new PlotCommand());
 
         initConfig();
 
         this.dbConnector = new MYSQLConnector(this);
-    }
-
-    private void initConfig() {
-        config.addDefault("db_ip", "127.0.0.1");
-        config.addDefault("db_name", "mcDatabase");
-        config.addDefault("db_username", "db_Username");
-        config.addDefault("db_password", "db_Password");
-        config.options().copyDefaults(true);
-        saveConfig();
-    }
-
-    public static MYSQLConnector GetDatabase() {
-        return dbConnector;
     }
 
     @Override
@@ -56,4 +46,12 @@ public class Oldoe extends JavaPlugin implements Listener {
         }
     }
 
+    private void initConfig() {
+        config.addDefault("db_ip", "127.0.0.1");
+        config.addDefault("db_name", "mcDatabase");
+        config.addDefault("db_username", "db_Username");
+        config.addDefault("db_password", "db_Password");
+        config.options().copyDefaults(true);
+        saveConfig();
+    }
 }
--- src/main/java/com/oldoe/plugin/commands/PlotCommand.java
+++ src/main/java/com/oldoe/plugin/commands/PlotCommand.java
@@ -3,11 +3,57 @@ package com.oldoe.plugin.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class PlotCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        return false;
+        if (sender instanceof  Player) {
+            Player player = (Player) sender;
+            String uuid = player.getUniqueId().toString();
+
+            if (args.length > 0) {
+                switch (args[0]) {
+                    case ("buy"):
+                        Buy(player);
+                        break;
+                    case ("sell"):
+                        Sell(player);
+                        break;
+                    case("list"):
+                        List(player);
+                        break;
+                    case("add"):
+                        Add(player);
+                        break;
+                    case("remove"):
+                        Remove(player);
+                        break;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private void Buy(Player player) {
+
+    }
+
+    private void Sell(Player player) {
+
+    }
+
+    private void List(Player player) {
+
+    }
+
+    private void Add(Player player) {
+
+    }
+
+    private void Remove(Player player) {
+
     }
 }
