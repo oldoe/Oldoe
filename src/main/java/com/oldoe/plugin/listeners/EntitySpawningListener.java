f4d5dd4295e838a3a2eeba273ab98d229abbbdcb
Spencer Burgess <40410913+SpencerBurgess@users.noreply.github.com>
2023-02-13 13:53:23 -0700

    Prevent all enemy mobs spawning near spawn

--- src/main/java/com/oldoe/plugin/listeners/EntitySpawningListener.java
+++ src/main/java/com/oldoe/plugin/listeners/EntitySpawningListener.java
@@ -1,8 +1,8 @@
 package com.oldoe.plugin.listeners;
 
 import org.bukkit.World;
+import org.bukkit.entity.Enemy;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Monster;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -15,7 +15,7 @@ public class EntitySpawningListener implements Listener {
         Entity entity = event.getEntity();
 
         // Hostile mobs that are wanting to spawn in the overworld
-        if (entity instanceof Monster && entity.getWorld().getEnvironment().equals(World.Environment.NORMAL)) {
+        if (entity instanceof Enemy && entity.getWorld().getEnvironment().equals(World.Environment.NORMAL)) {
             // Prevent hostile mobs from spawning within 128 blocks of spawn.
             if (Math.abs(entity.getLocation().getBlockX()) < 128 && Math.abs(entity.getLocation().getBlockZ()) < 128) {
                 event.setCancelled(true);
